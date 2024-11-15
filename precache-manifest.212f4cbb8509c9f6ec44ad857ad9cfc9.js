56613a32e038b4d2daba5ba43d7970030b5d2137
Francisco Layseca <layseca.plz@gmail.com>
2024-11-15 14:21:51 -0300

    Updates

---
+++ precache-manifest.212f4cbb8509c9f6ec44ad857ad9cfc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22e6b38147518c5927362382de6b6f7",
+    "url": "/Construdomotik/index.html"
+  },
+  {
+    "revision": "82edff6ce7a6392d0899",
+    "url": "/Construdomotik/static/css/main.62c918b1.chunk.css"
+  },
+  {
+    "revision": "1d7fe43a99b706912593",
+    "url": "/Construdomotik/static/js/2.bb0370a4.chunk.js"
+  },
+  {
+    "revision": "82edff6ce7a6392d0899",
+    "url": "/Construdomotik/static/js/main.d5f20beb.chunk.js"
+  },
+  {
+    "revision": "9195bcd451bc3c30cb2e",
+    "url": "/Construdomotik/static/js/runtime~main.fe7b83bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11a11d697c3b3b1b57164fef4bad89ea.js"
+  "/Construdomotik/precache-manifest.212f4cbb8509c9f6ec44ad857ad9cfc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Construdomotik/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
