48eb9b0acc385708e4e6ed429d683a6af42b3d24
Francisco Layseca <layseca.plz@gmail.com>
2024-11-16 14:03:37 -0300

    se separa home de app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Footer from './components/Footer';
 import Servicios from './components/Servicios';
 import Nosotros from './components/Nosotros';
 import Contacto from './components/Contacto';
+import Home from './components/Home';
 
-import './css/App.css';
 
 function App() {
   return (
@@ -18,57 +18,15 @@ function App() {
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
-   
+      
       <Footer />
     </Router>
   );
 }
 
-function Home() {
-  return (
-   
 
-    <section className="gallery-section">
-      <h2>Bienvenidos</h2>
-     
-      <div className="gallery-container">
-        <div className="gallery-item">
-          <img src="/images/con1.jpg" alt="Proyecto 1" />
-          <p>Proyecto de construcción inteligente</p>
-        </div>
-        <div className="gallery-item">
-        <img src={`${process.env.PUBLIC_URL}/images/con1.jpg`} alt="Proyecto 9" />
-          <p>Integración de domótica en el hogar</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con3.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con4.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con4.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con3.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con3.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-        <div className="gallery-item">
-          <img src="/images/con3.jpg" alt="Proyecto 3" />
-          <p>Renovación con tecnologías inteligentes</p>
-        </div>
-      </div>
-    </section>
     
-  );
-}
+  
 
 export default App;
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import '../css/Home.css';
+import '../css/Home.css'; // Importa el archivo de estilos de Home
 
 function Home() {
   return (
-   
-
     <section className="gallery-section">
       <h2>Bienvenidos</h2>
-     
       <div className="gallery-container">
         <div className="gallery-item">
-          <img src="/images/con1.jpg" alt="Proyecto 1" />
+          <img src={`${process.env.PUBLIC_URL}/images/con1.jpg`} alt="Proyecto 1" />
           <p>Proyecto de construcción inteligente</p>
         </div>
         <div className="gallery-item">
-          <img src="/images/con2.jpg" alt="Proyecto 2" />
+          <img src={`${process.env.PUBLIC_URL}/images/con2.jpg`} alt="Proyecto 9" />
           <p>Integración de domótica en el hogar</p>
         </div>
         <div className="gallery-item">
-          <img src="/images/con3.jpg" alt="Proyecto 3" />
+          <img src={`${process.env.PUBLIC_URL}/images/con3.jpg`} alt="Proyecto 3" />
           <p>Renovación con tecnologías inteligentes</p>
         </div>
         <div className="gallery-item">
-          <img src="/images/con4.jpg" alt="Proyecto 3" />
+          <img src={`${process.env.PUBLIC_URL}/images/con4.jpg`} alt="Proyecto 3" />
           <p>Renovación con tecnologías inteligentes</p>
         </div>
         <div className="gallery-item">
@@ -43,8 +40,7 @@ function Home() {
         </div>
       </div>
     </section>
-    
   );
 }
 
-export default App;
+export default Home;
